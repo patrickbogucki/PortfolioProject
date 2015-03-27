804192eb08647088c5694e2cb74cf2eff31d65dd
Patrick Bogucki <patrickbogucki@gmail.com>
2015-03-27 03:48:39 -0700

    Added floating logo that inverts color as it passes to a lighter colored background.

--- javascript/js1.js
+++ javascript/js1.js
@@ -1,11 +1,32 @@
 var main = function() {
-	var temp = $('.long-text').scrollTop();
+	// Scroll cover to reveal text below
 	var top = $('body');
 	$('.down').click(function() {
 		$('.long-text').ScrollTo({
 			duration: 1500,
 			erasing: 'linear'
-		});		
+		});
+		setTimeout(function() {
+			$('.logo, .logo a').addClass('logo-inverted');
+		}, 1500);		
+	});
+	if($(document).scrollTop() !== -1) {
+		console.log()
+	}
+	console.log($(document).scrollTop());
+
+	// Keep logo visible during scrolling
+	$(window).scroll(function() {
+		
+		// console.log(window.innerHeight);
+	
+
+		if($(document).scrollTop() > window.innerHeight) {
+			$('.logo, .logo a').addClass('logo-inverted');
+		}
+		else if($(document).scrollTop() < window.innerHeight) {
+			$('.logo').removeClass('logo-inverted');
+		}
 	});
 };
 
